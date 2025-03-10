a35aa66eabe8de351e1904182d9a7c0d83b09da4
thebcrayon <106349257+thebcrayon@users.noreply.github.com>
2025-03-09 19:13:21 -0500

    Complete #stock_picker implementation

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,10 +1,29 @@
-# Implement a method #stock_picker that takes
-# in an array of stock prices, one for each
-# hypothetical day. It should return a pair
-# of days representing the best day to buy
-# and the best day to sell. Days start at 0.
 def stock_picker(days_array)
-  "hello"
+  # set initial values
+  highest_profit = 0
+  best_buy = 0
+  best_sell = 0
+
+  # Gets us each price of the array as a buying price
+  days_array.each_with_index do |buy_price, idx|
+    
+    # Gives us selling possibilities from only current index forward
+    days_array[idx..days_array.size].each_with_index do |sell_price, sell_index|
+      
+      # Track current profit and change it if necessary
+      current_profit = sell_price - buy_price
+      if current_profit > highest_profit
+        highest_profit = current_profit
+        best_buy = idx
+        best_sell = sell_index + idx # Needs to reference index in context of days_array
+      end
+    end
+  end
+
+  # Implicit return
+  # best_buy best_sell are storing indexes in reference to original days_array
+  "Buying at $#{days_array[best_buy]} and selling at $#{days_array[best_sell]} will yield the highest profit of $#{days_array[best_sell] - days_array[best_buy]}"
+
 end
 
-p stock_picker(Array.new(3))
+puts stock_picker([17,3,6,9,15,8,6,1,10])
